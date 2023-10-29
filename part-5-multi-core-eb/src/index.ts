0ba7eb9ff183882967231ee035fab6c199aa7c1f
Harkirat Singh <harkirat96@gmail.com>
2023-10-29 16:10:00 +0530

    added workflow

---
+++ part-5-multi-core-eb/src/index.ts
@@ -0,0 +1,19 @@
+import cluster from "cluster";
+import os from "os";
+import { app } from "./express-app";
+
+const port = process.env.PORT || 3000;
+
+const cpuCount = os.cpus().length;
+
+if (cluster.isMaster) {
+  // Create a worker for each CPU
+  for (let i = 0; i < cpuCount; i++) {
+    cluster.fork();
+  }
+} else {
+  app.listen(port, () => {
+    console.log("listening on port 3000");
+  });
+}
+
